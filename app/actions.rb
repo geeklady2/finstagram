fa016b0c9f432b2ae68394f2ec18c4b1281ac7ca
geeklady2 <sdjaeger@gmail.com>
2021-05-27 00:05:25 +0000

    Method exercise

--- app/actions.rb
+++ app/actions.rb
@@ -1,3 +1,11 @@
+def humanized_time_ago(time_ago_in_minutes) 
+    if time_ago_in_minutes >= 60
+        "#{time_ago_in_minutes / 60} hours ago"
+    else
+        "#{time_ago_in_minutes} minutes ago"
+    end
+end
+
 get '/' do
     username = "sharky_j"
     avatar_url = "http://naserca.com/images/sharky_j.jpg"
@@ -9,10 +17,6 @@ get '/' do
         "sharky_j: Out for the long weekend ... too embarrassed to show y'all the beach bod!"
     ]
     
-    if time_ago_in_minutes > 60
-        "#{time_ago_in_minutes / 60} hours ago"
-    else
-        "#{time_ago_in_minutes} minutes ago"
-    end
+    humanized_time_ago(time_ago_in_minutes)
     #File.read(File.join('app/views', 'index.html'))
 end
